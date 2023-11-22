1336e1f6985b38f3d97c4fb2bdeb5bf40c856a4f
William Davidson <wj.davidson@outlook.com>
2023-11-22 14:19:18 +0100

    recently searched onClick search + delete from list

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ let deviceId = "";
 
 const baseUrl = "https://api.spotify.com/v1";
 
-const authSpotify = () => {
-  const generateRandomString = (length) => {
+function authSpotify() {
+  function generateRandomString(length) {
     const possible =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const values = crypto.getRandomValues(new Uint8Array(length));
     return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-  };
+  }
 
-  const generateCodeChallenge = async (codeVerifier) => {
+  async function generateCodeChallenge(codeVerifier) {
     const digest = await crypto.subtle.digest(
       "SHA-256",
       new TextEncoder().encode(codeVerifier)
@@ -28,16 +28,16 @@ const authSpotify = () => {
       .replace(/=/g, "")
       .replace(/\+/g, "-")
       .replace(/\//g, "_");
-  };
+  }
 
-  const generateUrlWithSearchParams = (url, params) => {
+  function generateUrlWithSearchParams(url, params) {
     const urlObject = new URL(url);
     urlObject.search = new URLSearchParams(params).toString();
 
     return urlObject.toString();
-  };
+  }
 
-  const redirectToSpotifyAuthorizeEndpoint = () => {
+  function redirectToSpotifyAuthorizeEndpoint() {
     const codeVerifier = generateRandomString(64);
 
     generateCodeChallenge(codeVerifier).then((code_challenge) => {
@@ -56,9 +56,9 @@ const authSpotify = () => {
         }
       );
     });
-  };
+  }
 
-  const exchangeToken = (code) => {
+  function exchangeToken(code) {
     const code_verifier = localStorage.getItem("code_verifier");
 
     fetch("https://accounts.spotify.com/api/token", {
@@ -82,9 +82,9 @@ const authSpotify = () => {
         window.history.replaceState({}, document.title, "/");
       })
       .catch((error) => console.error(error));
-  };
+  }
 
-  const processTokenResponse = (data) => {
+  function processTokenResponse(data) {
     const { access_token, refresh_token, expires_in } = data;
 
     const time = new Date();
@@ -93,20 +93,24 @@ const authSpotify = () => {
     localStorage.setItem("access_token", access_token);
     localStorage.setItem("refresh_token", refresh_token);
     localStorage.setItem("expires_at", expires_at);
-  };
+  }
 
   const refreshToken = () => {
     fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body: new URLSearchParams({
         client_id: clientId,
         grant_type: "refresh_token",
         refresh_token: localStorage.getItem("refresh_token"),
       }),
-    });
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        processTokenResponse(data);
+      });
   };
 
   const args = new URLSearchParams(window.location.search);
@@ -116,9 +120,137 @@ const authSpotify = () => {
 
   loginBtn.addEventListener("click", redirectToSpotifyAuthorizeEndpoint);
   refreshTokenBtn.addEventListener("click", refreshToken);
-};
+}
 
-const getArtistTopTracks = async (id) => {
+function initPlaybackSdk() {
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = localStorage.getItem("access_token");
+    const player = new Spotify.Player({
+      name: "Client baserad utveckling Spotify",
+      getOAuthToken: (cb) => {
+        cb(token);
+      },
+      volume: 0.5,
+    });
+
+    // Ready
+    player.addListener("ready", ({ device_id }) => {
+      deviceId = device_id;
+      console.log("Ready with Device ID", device_id);
+    });
+
+    // Not Ready
+    player.addListener("not_ready", ({ device_id }) => {
+      console.log("Device ID has gone offline", device_id);
+    });
+
+    player.addListener("initialization_error", ({ message }) => {
+      console.error(message);
+    });
+
+    player.addListener("authentication_error", ({ message }) => {
+      console.error(message);
+    });
+
+    player.addListener("account_error", ({ message }) => {
+      console.error(message);
+    });
+
+    player.connect();
+  };
+}
+
+function createSearchArtistCard(artist) {
+  const artistContainer = document.createElement("div");
+  artistContainer.classList.add("artist-container", "result-artist-container");
+
+  const artistProfileContainer = document.createElement("div");
+  artistProfileContainer.classList.add("artist-profile-container");
+
+  const artistImg = document.createElement("img");
+  artistImg.src = artist.image;
+  artistImg.classList.add("profile-img");
+
+  const artistName = document.createElement("h3");
+  artistName.textContent = artist.name;
+
+  const playBtn = document.createElement("button");
+  playBtn.classList.add("play-icon");
+  playBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+
+  artistProfileContainer.append(artistImg, artistName);
+  artistContainer.append(artistProfileContainer, playBtn);
+  resultContainer.appendChild(artistContainer);
+}
+
+function createArtistTopTracks(tracks) {
+  const topTracksContainer = document.createElement("div");
+  topTracksContainer.classList.add("top-tracks-container");
+
+  tracks.forEach((track) => {
+    const trackContainer = document.createElement("div");
+    trackContainer.classList.add("track-container");
+    trackContainer.addEventListener("click", () => {
+      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+        body: JSON.stringify({
+          uris: [track.uri],
+          position_ms: 0,
+        }),
+      });
+    });
+
+    const trackNameContainer = document.createElement("div");
+
+    const trackName = document.createElement("span");
+    trackName.classList.add("track-name");
+    trackName.textContent = track.name;
+
+    const trackArtistsContainer = document.createElement("div");
+    trackArtistsContainer.classList.add("track-artists-container");
+
+    const explicit = document.createElement("div");
+    explicit.classList.add("explicit");
+    explicit.textContent = "E";
+    if (track.explicit) {
+      explicit.style.display = "none";
+    }
+
+    trackArtistsContainer.appendChild(explicit);
+
+    track.artists.forEach((artist) => {
+      const artistName = document.createElement("a");
+      artistName.href = artist.external_urls.Spotify;
+      artistName.textContent = artist.name;
+      artistName.classList.add("artist-name-sm");
+
+      trackArtistsContainer.appendChild(artistName);
+    });
+
+    // convert milliseconds to seconds
+    let seconds = Math.floor(track.duration_ms / 1000);
+    // calc minutes
+    let minutes = Math.floor(seconds / 60);
+    // assign remaining seconds
+    seconds %= 60;
+
+    const trackDuration = document.createElement("div");
+    trackDuration.classList.add("track-time");
+    trackDuration.textContent = `${String(minutes).padStart(2, "0")}:${String(
+      seconds
+    ).padStart(2, "0")}`;
+
+    trackNameContainer.append(trackName, trackArtistsContainer);
+    trackContainer.append(trackNameContainer, trackDuration);
+    topTracksContainer.appendChild(trackContainer);
+  });
+  resultContainer.appendChild(topTracksContainer);
+}
+
+async function getArtistTopTracks(id) {
   try {
     const response = await fetch(
       `${baseUrl}/artists/${id}/top-tracks?market=ES`,
@@ -131,79 +263,11 @@ const getArtistTopTracks = async (id) => {
     );
     const { tracks } = await response.json();
 
-    const topTracksContainer = document.createElement("div");
-    topTracksContainer.classList.add("top-tracks-container");
-
-    tracks.forEach((track, index) => {
-      if (index >= 4) return;
-
-      const trackContainer = document.createElement("div");
-      trackContainer.classList.add("track-container");
-      trackContainer.addEventListener("click", () => {
-        fetch(
-          `https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`,
-          {
-            method: "PUT",
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-            },
-            body: JSON.stringify({
-              uris: [track.uri],
-              position_ms: 0,
-            }),
-          }
-        );
-      });
-
-      const trackNameContainer = document.createElement("div");
-
-      const trackName = document.createElement("span");
-      trackName.classList.add("track-name");
-      trackName.textContent = track.name;
-
-      const trackArtistsContainer = document.createElement("div");
-      trackArtistsContainer.classList.add("track-artists-container");
-
-      const explicit = document.createElement("div");
-      explicit.classList.add("explicit");
-      explicit.textContent = "E";
-      if (track.explicit) {
-        explicit.style.display = "none";
-      }
-
-      trackArtistsContainer.appendChild(explicit);
-
-      track.artists.forEach((artist) => {
-        const artistName = document.createElement("a");
-        artistName.href = artist.external_urls.Spotify;
-        artistName.textContent = artist.name;
-        artistName.classList.add("artist-name-sm");
-
-        trackArtistsContainer.appendChild(artistName);
-      });
-
-      // convert milliseconds to seconds
-      let seconds = Math.floor(track.duration_ms / 1000);
-      // calc minutes
-      let minutes = Math.floor(seconds / 60);
-      // assign remaining seconds
-      seconds %= 60;
-
-      const trackDuration = document.createElement("div");
-      trackDuration.classList.add("track-time");
-      trackDuration.textContent = `${String(minutes).padStart(2, "0")}:${String(
-        seconds
-      ).padStart(2, "0")}`;
-
-      trackNameContainer.append(trackName, trackArtistsContainer);
-      trackContainer.append(trackNameContainer, trackDuration);
-      topTracksContainer.appendChild(trackContainer);
-    });
-    resultContainer.appendChild(topTracksContainer);
+    createArtistTopTracks(tracks.splice(0, 4));
   } catch (error) {
     console.error(error);
   }
-};
+}
 
 const searchArtist = async (search) => {
   try {
@@ -219,7 +283,7 @@ const searchArtist = async (search) => {
       `${baseUrl}/search?q=${search}&type=artist,album&limit=1`,
       params
     );
-    const { artists, albums } = await response.json();
+    const { artists } = await response.json();
 
     // Save artist to local storage
     let recentSearch = JSON.parse(localStorage.getItem("recentSearch")) || [];
@@ -239,38 +303,23 @@ const searchArtist = async (search) => {
       JSON.stringify([artist, ...recentSearch])
     );
 
-    getArtistTopTracks(artists.items[0].id);
+    createRecentlySearched();
 
     resultContainer.innerHTML = "";
 
-    const artistContainer = document.createElement("div");
-    artistContainer.classList.add("artist-container");
-
-    const artistProfileContainer = document.createElement("div");
-    artistProfileContainer.classList.add("artist-profile-container");
-
-    const artistImg = document.createElement("img");
-    artistImg.src = artists.items[0].images[1].url;
-    artistImg.classList.add("profile-img");
-
-    const artistName = document.createElement("h3");
-    artistName.textContent = artists.items[0].name;
-
-    const playBtn = document.createElement("button");
-    playBtn.classList.add("play-icon");
-    playBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
-
-    artistProfileContainer.append(artistImg, artistName);
-    artistContainer.append(artistProfileContainer, playBtn);
-    resultContainer.appendChild(artistContainer);
+    getArtistTopTracks(artists.items[0].id);
+    const { images, name } = artists.items[0];
+    createSearchArtistCard({ name, image: images[1].url });
   } catch (error) {
     console.error(error);
   }
 };
 
-// Create recent search cards
-const recentlySearched = JSON.parse(localStorage.getItem("recentSearch")) || [];
-if (recentlySearched.length > 0) {
+function createRecentlySearched() {
+  const recentlySearched =
+    JSON.parse(localStorage.getItem("recentSearch")) || [];
+  if (recentlySearched.length === 0) return;
+  recentSearch.innerHTML = "";
   const containerTitle = document.createElement("h1");
   containerTitle.textContent = "Recently searched";
   containerTitle.classList.add("section-title");
@@ -285,12 +334,25 @@ if (recentlySearched.length > 0) {
       "artist-profile-container",
       "recent-card"
     );
+    card.addEventListener("click", () => {
+      searchArtist(artist.name);
+    });
 
     const imgContainer = document.createElement("div");
     imgContainer.classList.add("recent-img-container");
 
     const x = document.createElement("i");
     x.classList.add("fa-solid", "fa-x", "recent-x");
+    x.addEventListener("click", (ev) => {
+      ev.stopPropagation();
+      const recentStorage = JSON.parse(localStorage.getItem("recentSearch"));
+
+      const removedRecentCard = recentStorage.filter(
+        (search) => search.name !== artist.name
+      );
+      localStorage.setItem("recentSearch", JSON.stringify(removedRecentCard));
+      createRecentlySearched();
+    });
 
     const profileImg = document.createElement("img");
     profileImg.classList.add("profile-img");
@@ -307,43 +369,6 @@ if (recentlySearched.length > 0) {
   recentSearch.append(containerTitle, recentCardsContainer);
 }
 
-// Playback sdk
-window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = localStorage.getItem("access_token");
-  const player = new Spotify.Player({
-    name: "Client baserad utveckling Spotify",
-    getOAuthToken: (cb) => {
-      cb(token);
-    },
-    volume: 0.5,
-  });
-
-  // Ready
-  player.addListener("ready", ({ device_id }) => {
-    deviceId = device_id;
-    console.log("Ready with Device ID", device_id);
-  });
-
-  // Not Ready
-  player.addListener("not_ready", ({ device_id }) => {
-    console.log("Device ID has gone offline", device_id);
-  });
-
-  player.addListener("initialization_error", ({ message }) => {
-    console.error(message);
-  });
-
-  player.addListener("authentication_error", ({ message }) => {
-    console.error(message);
-  });
-
-  player.addListener("account_error", ({ message }) => {
-    console.error(message);
-  });
-
-  player.connect();
-};
-
 searchForm.addEventListener("submit", (ev) => {
   ev.preventDefault();
   const input = searchForm.querySelector("#artist-input");
@@ -351,3 +376,5 @@ searchForm.addEventListener("submit", (ev) => {
 });
 
 authSpotify();
+initPlaybackSdk();
+createRecentlySearched();
